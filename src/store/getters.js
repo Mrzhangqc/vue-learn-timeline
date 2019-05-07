8fcf0cd70ebc1784a3da411d4c4dfef428c565b8
Mr_Zhang <henu1314@163.com>
2019-05-07 13:49:15 +0800

    add static file

--- src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  sidebar: state => state.sidebar,
-  token: state => state.token,
-  permission_routes: state => state.permission_routes,
-  breadcrumb: state => state.breadcrumb
-}
-
-export default getters
