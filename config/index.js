490701a64e2963b1c8a8f43281cd36d505d1a1cd
Mr_Zhang <henu1314@163.com>
2019-05-07 14:22:17 +0800

    update webpack proxyTable config

--- config/index.js
+++ config/index.js
@@ -16,8 +16,8 @@ module.exports = {
         target: function (env) {
           switch (env) {
             case 'production':
-              return 'http://localhost:8080/';
-          
+             // return 'http://localhost:8080/';
+             return 'http://74.82.199.235:8088/api/';
             default:
               return 'http://74.82.199.235:8088/api/';
           }
