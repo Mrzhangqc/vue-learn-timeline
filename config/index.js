2e17283024bf47785ae87c0122d83c8d0269f008
Mr_Zhang <henu1314@163.com>
2019-05-08 14:39:14 +0800

    add gh-pages package 自动打包发布

--- config/index.js
+++ config/index.js
@@ -80,7 +80,7 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false, // npm run build 打包后，js文件会出现.map文件，文件很大很占空间，这里设置为false打包即不会出现.map文件
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
