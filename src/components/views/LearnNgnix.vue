8d58bbff196710e9123ead3dc685c4fa1644d2e3
Mr_Zhang <henu1314@163.com>
2019-08-15 11:00:58 +0800

    test component LifeCycle

--- src/components/views/LearnNgnix.vue
+++ src/components/views/LearnNgnix.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>learn ngnix</div>
+    <div ref="div">{{msg}}</div>
 </template>
 
 <script>
@@ -7,7 +7,41 @@
 export default {
   name: 'LearnNgnix',
   data () {
-    return {}
+    return {
+      msg: 'message'
+    }
+  },
+  methods: {
+    changeMsg: function () {
+      setTimeout(() => {
+        this.msg = 'message-updated'
+      }, 1000)
+    }
+  },
+  beforeCreate () {
+    console.log('beforeCreate-', this.$el, this.$data, this.msg)
+  },
+  created () {
+    this.changeMsg()
+    console.log('created-', this.$el, this.$data, this.msg)
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate-', this.$el, this.$data, this.msg)
+  },
+  updated () {
+    console.log('updated-', this.$el, this.$data, this.msg)
+  },
+  beforeMount () {
+    console.log('beforeMount-', this.$el, this.$data, this.msg)
+  },
+  mounted () {
+    console.log('mounted-', this.$el, this.$data, this.msg)
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy-', this.msg)
+  },
+  destroyed () {
+    console.log('destroyed-', this.$el)
   }
 }
 </script>
